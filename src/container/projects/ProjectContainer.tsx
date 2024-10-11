298dbb177f60021a9e75ec8c20f437cb7c17d653
Brajnam <mrbrajnam@gmail.com>
2024-10-11 11:02:41 +0530

    Boiler Plate created

---
+++ src/container/projects/ProjectContainer.tsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const ProjectContainer = () => {
+  return (
+    <div>ProjectContainer</div>
+  )
+}
+
+export default ProjectContainer
---
+++ src/utils/constants.ts
@@ -0,0 +1,39 @@
+import HomePage from "../pages/home/HomePage";
+import ContactPage from "../pages/contact/ContactPage";
+import AboutPage from "../pages/about/AboutPage";
+import ProjectPage from "../pages/projects/ProjectPage";
+
+export interface RouteMaps {
+    __id: string;
+    name: string;
+    URL: string;
+    element:() => JSX.Element
+}
+
+
+export const routeMap:RouteMaps[] = [
+    {
+        __id: '1',
+        name: 'Home',
+        URL: '/',
+        element: HomePage
+    },
+    {
+        __id: '2',
+        name: 'Contact',
+        URL: '/contact',
+        element: ContactPage
+    },
+    {
+        __id: '3',
+        name: 'About',
+        URL: '/about',
+        element: AboutPage
+    },
+    {
+        __id: '4',
+        name: 'Projects',
+        URL: '/projects',
+        element: ProjectPage
+    },
+]
